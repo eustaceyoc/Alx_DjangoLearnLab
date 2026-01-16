a43b8af4621b3c8f42f64c279c516d5bd80c7f5d
Eustace Crentsil <crentsil.eustace25@gmail.com>
2026-01-16 18:29:16 +0000

    Task 4

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -6,6 +6,9 @@ from django.shortcuts import redirect
 from django.contrib.auth import login
 from .models import Book
 from .models import Library
+from django.contrib.auth.decorators import user_passes_test
+from django.http import HttpResponse
+
 
 # Function-based view: render template with all books
 def list_books(request):
@@ -27,3 +30,29 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, 'relationship_app/register.html', {'form': form})
+
+def is_admin(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Admin'
+
+
+def is_librarian(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Librarian'
+
+
+def is_member(user):
+    return hasattr(user, 'userprofile') and user.userprofile.role == 'Member'
+
+
+@user_passes_test(is_admin)
+def admin_view(request):
+    return HttpResponse("Admin content")
+
+
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return HttpResponse("Librarian content")
+
+
+@user_passes_test(is_member)
+def member_view(request):
+    return HttpResponse("Member content")
