455eaefb1dbd76c282e4fd1b268d30cd2d4fac50
Eustace Crentsil <crentsil.eustace25@gmail.com>
2026-01-16 18:55:22 +0000

    Task 5 redo #1

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -21,5 +21,4 @@ urlpatterns = [
     path('books/add/', views.add_book, name='add_book'),
     path('books/edit/<int:pk>/', views.edit_book, name='edit_book'),
     path('books/delete/<int:pk>/', views.delete_book, name='delete_book'),
-
 ]
