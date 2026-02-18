2c24456b8cd3fea016909b9f00cf9b22b406bfb1
Eustace Crentsil <crentsil.eustace25@gmail.com>
2026-02-18 17:10:52 +0000

    Task 1. Correction 2

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,46 +1,94 @@
-from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
+from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.utils.translation import gettext_lazy as _
 from django.conf import settings
 
-# Step 1a: Custom user manager
+
 class CustomUserManager(BaseUserManager):
-    def create_user(self, username, email, date_of_birth=None, password=None, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
         if not email:
-            raise ValueError("Users must have an email address")
+            raise ValueError(_('The Email field must be set'))
         email = self.normalize_email(email)
-        user = self.model(username=username, email=email, date_of_birth=date_of_birth, **extra_fields)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, username, email, password=None, **extra_fields):
+    def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_active', True)
 
-        return self.create_user(username, email, password=password, **extra_fields)
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError(_('Superuser must have is_staff=True.'))
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError(_('Superuser must have is_superuser=True.'))
 
-# Step 1b: Custom user model
+        return self.create_user(email, password, **extra_fields)
+
+
 class CustomUser(AbstractUser):
-    date_of_birth = models.DateField(null=True, blank=True)
-    profile_photo = models.CharField(max_length=255, null=True, blank=True)
+    username = None
+    email = models.EmailField(_('email address'), unique=True)
+    date_of_birth = models.DateField(_('date of birth'), null=True, blank=True)
+    profile_photo = models.ImageField(
+        _('profile photo'),
+        upload_to='profile_photos/',
+        null=True,
+        blank=True
+    )
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['first_name', 'last_name']
+
     objects = CustomUserManager()
 
     def __str__(self):
-        return self.username
+        return self.email
+
+    class Meta:
+        verbose_name = _('user')
+        verbose_name_plural = _('users')
 
 class Book(models.Model):
-    title = models.CharField(max_length=255)
-    author = models.CharField(max_length=255)
-    published_date = models.DateField()
-    added_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    description = models.TextField(blank=True)
+    published_date = models.DateField(null=True, blank=True)
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='books_created'
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     class Meta:
         permissions = [
-            ("can_view", "Can view article"),
-            ("can_create", "Can create article"),
-            ("can_edit", "Can edit article"),
-            ("can_delete", "Can delete article"),
+            ("can_view_book", "Can view book"),
+            ("can_create_book", "Can create book"),
+            ("can_edit_book", "Can edit book"),
+            ("can_delete_book", "Can delete book"),
         ]
 
     def __str__(self):
         return self.title
+
+class Library(models.Model):
+    name = models.CharField(max_length=100)
+    books = models.ManyToManyField(Book, related_name='libraries', blank=True)
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='libraries_created'
+    )
+
+    class Meta:
+        permissions = [
+            ("can_view_library", "Can view library"),
+            ("can_manage_library", "Can manage library"),
+        ]
+
+    def __str__(self):
+        return self.name
--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,29 +1,98 @@
-from django.shortcuts import render, get_object_or_404
-from django.contrib.auth.decorators import permission_required
-from .models import Article
+from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required, permission_required
+from django.contrib import messages
+from django.http import HttpResponseForbidden, HttpResponseBadRequest
+from django.db.models import Q
+from .models import Book, Library
+from .forms import ExampleForm
 
-# View article
-@permission_required('articles.can_view', raise_exception=True)
-def view_article(request, pk):
-    article = get_object_or_404(Article, pk=pk)
-    return render(request, 'articles/view_article.html', {'article': article})
 
-# Create article
-@permission_required('articles.can_create', raise_exception=True)
-def create_article(request):
-    # your create logic here
-    pass
+@login_required
+@permission_required('bookshelf.can_view_book', raise_exception=True)
+def book_list(request):
+    search_query = request.GET.get('search', '').strip()
 
-# Edit article
-@permission_required('articles.can_edit', raise_exception=True)
-def edit_article(request, pk):
-    article = get_object_or_404(Article, pk=pk)
-    # your edit logic here
-    pass
+    if search_query:
+        books = Book.objects.filter(
+            Q(title__icontains=search_query) |
+            Q(author__icontains=search_query)
+        )
+    else:
+        books = Book.objects.all()
 
-# Delete article
-@permission_required('articles.can_delete', raise_exception=True)
-def delete_article(request, pk):
-    article = get_object_or_404(Article, pk=pk)
-    # your delete logic here
-    pass
+    return render(request, 'bookshelf/book_list.html', {
+        'books': books,
+        'search_query': search_query
+    })
+
+
+@login_required
+@permission_required('bookshelf.can_create_book', raise_exception=True)
+def book_create(request):
+    if request.method == 'POST':
+        form = ExampleForm(request.POST)
+        if form.is_valid():
+            # Secure creation using Django ORM with validated data
+            book = form.save(commit=False)
+            book.created_by = request.user
+            book.save()
+            messages.success(request, f'Book "{book.title}" created successfully!')
+            return redirect('book_list')
+    else:
+        form = ExampleForm()
+
+    return render(request, 'bookshelf/form_example.html', {'form': form})
+
+
+@login_required
+@permission_required('bookshelf.can_edit_book', raise_exception=True)
+def book_edit(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+
+    if book.created_by != request.user and not request.user.is_superuser:
+        return HttpResponseForbidden("You can only edit books you created.")
+
+    if request.method == 'POST':
+        form = ExampleForm(request.POST, instance=book)
+        if form.is_valid():
+            form.save()
+            messages.success(request, f'Book "{book.title}" updated successfully!')
+            return redirect('book_list')
+    else:
+        form = ExampleForm(instance=book)
+
+    return render(request, 'bookshelf/form_example.html', {'form': form, 'book': book})
+
+@login_required
+@permission_required('bookshelf.can_delete_book', raise_exception=True)
+def book_delete(request, book_id):
+    book = get_object_or_404(Book, id=book_id)
+
+    if book.created_by != request.user and not request.user.is_superuser:
+        return HttpResponseForbidden("You can only delete books you created.")
+
+    if request.method == 'POST':
+        book_title = book.title
+        book.delete()
+        messages.success(request, f'Book "{book_title}" deleted successfully!')
+        return redirect('book_list')
+
+    return render(request, 'bookshelf/book_confirm_delete.html', {'book': book})
+
+
+@login_required
+@permission_required('bookshelf.can_view_library', raise_exception=True)
+def library_list(request):
+    libraries = Library.objects.all()
+    return render(request, 'bookshelf/library_list.html', {'libraries': libraries})
+
+
+def user_dashboard(request):
+    user_books = Book.objects.filter(created_by=request.user)
+    user_permissions = request.user.get_all_permissions()
+
+    return render(request, 'bookshelf/dashboard.html', {
+        'user_books': user_books,
+        'user_permissions': user_permissions
+    })
