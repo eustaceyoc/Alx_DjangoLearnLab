08c289857d614989fe26b85db676b252b6675185
Eustace Crentsil <crentsil.eustace25@gmail.com>
2026-02-25 22:33:27 +0000

    Task 2

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,29 +1,35 @@
 from django.urls import path
+from . import views
 from .views import (
-    PostListView,
-    PostDetailView,
-    PostCreateView,
-    PostUpdateView,
-    PostDeleteView
+    PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
 )
 
+
 urlpatterns = [
+    path('register/', views.register_view, name = 'register'),
+    path('login/', views.login_view, name='login'),
+    path('logout/', views.logout_view, name='logout'),
+    path('profile/', views.profile_view, name='profile'),
+]
 
-    path('', PostListView.as_view(), name='post-list'),
+urlpatterns += [
+    path('posts/', PostListView.as_view(), name='post-list'),
+    path('post/new/', PostCreateView.as_view(), name='post-create'),           # changed 'posts/new/' → 'post/new/'
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('post/<int:pk>/update/', PostUpdateView.as_view(), name='post-update'),  # changed 'edit' → 'update'
+    path('post/<int:pk>/delete/', PostDeleteView.as_view(), name='post-delete'),
+]
 
-    path('posts/<int:pk>/',
-         PostDetailView.as_view(),
-         name='post-detail'),
+from .views import CommentCreateView, CommentUpdateView, CommentDeleteView
 
-    path('posts/new/',
-         PostCreateView.as_view(),
-         name='post-create'),
+urlpatterns += [
+    path('post/<int:pk>/comments/new/', CommentCreateView.as_view(), name='comment-create'),
+    path('comment/<int:pk>/update/', CommentUpdateView.as_view(), name='comment-update'),
+    path('comment/<int:pk>/delete/', CommentDeleteView.as_view(), name='comment-delete'),
+]
 
-    path('posts/<int:pk>/edit/',
-         PostUpdateView.as_view(),
-         name='post-update'),
+from .views import PostByTagListView
 
-    path('posts/<int:pk>/delete/',
-         PostDeleteView.as_view(),
-         name='post-delete'),
+urlpatterns += [
+    path('tags/<slug:tag_slug>/', PostByTagListView.as_view(), name='posts-by-tag'),
 ]
