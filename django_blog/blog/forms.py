cbadd0d308324eb21aa7d3849890ce520bc0dbbb
Eustace Crentsil <crentsil.eustace25@gmail.com>
2026-02-25 23:00:15 +0000

    Task 4.1

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,56 +1,32 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
-from .models import Post, Comment
+from .models import Post
+from taggit.forms import TagWidget
 
-
-class RegisterForm(UserCreationForm):
+class CustomeUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
 
     class Meta:
         model = User
-        fields = ["username", "email", "password1", "password2"]
+        fields = ("username", "email", "password1", "password2")
 
-
-class ProfileForm(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ["username", "email"]
 
 class PostForm(forms.ModelForm):
-
-    tags = forms.CharField(
-        required=False,
-        help_text="Enter tags separated by commas"
-    )
-
     class Meta:
-        model = Post
-        fields = ['title', 'content', 'tags']
+        model =Post
+        fields = ["title", "content", "tags"]
+        widgets = {
+            'tags': TagWidget(), 
+        }
 
-def save(self, commit=True):
-
-        post = super().save(commit=False)
-
-        if commit:
-            post.save()
-
-            tag_names = self.cleaned_data['tags'].split(',')
-
-            for tag_name in tag_names:
-
-                tag_name = tag_name.strip()
-
-                if tag_name:
-
-                    tag, created = Tag.objects.get_or_create(name=tag_name)
-
-                    post.tags.add(tag)
-
-        return post    
+# comment form
+from .models import Comment
 
 class CommentForm(forms.ModelForm):
-
     class Meta:
         model = Comment
         fields = ['content']
+        widgets = {
+            'content': forms.Textarea(attrs={'rows': 3, 'placeholder': 'Write a comment...'}),
+        }
